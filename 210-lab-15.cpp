0991c1edfd7b04f639fd4cff48a12166b88a9d15
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-24 18:07:00 -0700

    read and output data

--- 210-lab-15.cpp
+++ 210-lab-15.cpp
@@ -36,19 +36,27 @@ int main() {
     string title; // temporarly holds the title
     int year; // temporarly holds the year
     string screenWriter; // temporarly holds the screenWriter
-
     if(inputFile.good()) { 
-        while (inputFile >> title) { 
-            inputFile.ignore();
+        while (getline(inputFile, title)) { 
+            //inputFile.ignore();
             inputFile >> year; 
+            inputFile.ignore();
             getline(inputFile, screenWriter);
             Movie tempMovie; 
             tempMovie.setTitle(title);
             tempMovie.setYearReleased(year);
             tempMovie.setWriterName(screenWriter);
+            movies.push_back(tempMovie);
         }
+        inputFile.close();
+    }
+    else { 
+        cout << "File not found" << endl; 
     }
 
+    for (auto movie : movies) { 
+        movie.print(); 
+    }
 
     return 0; 
 }
