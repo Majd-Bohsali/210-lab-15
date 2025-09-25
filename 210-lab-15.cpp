c1d98340af99bcc3db30bbc69b07c0b9ac77dc24
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-24 17:52:33 -0700

    Wrote Movie Class

--- 210-lab-15.cpp
+++ 210-lab-15.cpp
@@ -3,7 +3,30 @@
 
 #include <iostream>
 #include <vector> 
+#include <iomanip>
 using namespace std;
+
+const int W15 = 15; 
+
+class Movie {
+private:
+    int yearReleased; 
+    string title; 
+    string writerName; 
+public: 
+    int getYearReleased()               { return yearReleased; }
+    void setYearReleased(int year)      { yearReleased = year; }
+    string getTitle()                   { return title; }
+    void setTitle(string movieTitle)    { title = movieTitle; }
+    string getWriterName()              { return writerName; }
+    void setWriterName(int n)           { writerName = n; }
+
+    void print() { 
+        cout << "Movie: " << title << endl; 
+        cout << setw(W15) << "Year Released: " << yearReleased << endl; 
+        cout << setw(W15) << "Screenwriter: " << writerName << endl; 
+    }
+};
 
 int main() { 
     return 0; 
